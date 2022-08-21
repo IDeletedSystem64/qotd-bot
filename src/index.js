bb9d120d4e2fa3cb8168da9dca56151d25081f0f
Sophie Marie <sylveon64@hotmail.com>
2022-08-21 15:05:36 -0500

    Move settings functions to their own file, change so code uses them from file instead

--- src/index.js
+++ src/index.js
@@ -1,37 +1,13 @@
 const common = require("./common.js");
+const settings = require("../funcs/settings.js");
 
 const Discord = require("discord.js");
 
-const fs = require("fs");
-const path = require("path");
-
 var account = common.readJsonSync("account.json"); // private bot info
-var config = common.readJsonSync("config.json"); // global config
 
 var client = new Discord.Client({ intents: [Discord.GatewayIntentBits.Guilds] });
 
-var settings = new Map(); // server ID -> settings object
 
-function writeSettings(guildId, data) {
-	fs.writeFileSync(path.join(config.dataDir, `${guildId}.json`), JSON.stringify(data));
-}
-function readSettings(guildId) {
-	// Why?
-	try {
-		fs.accessSync(path.join(config.dataDir, `${guildId}.json`), fs.constants.F_OK);
-		// File does exist
-		settings.set(guildId, JSON.parse(fs.readFileSync(path.join(config.dataDir, `${guildId}.json`))));
-	} catch { // File doesn't exist
-		writeSettings(guildId, config.defaultSettings);
-		settings.set(guildId, config.defaultSettings);
-	}
-}
-function getSettings(guildId) {
-	return settings.get(guildId);
-}
-function saveSettings(guildId) {
-	writeSettings(guildId, getSettings(guildId));
-}
 function formatQuotes(quotes, page) {
 	if (quotes.length === 0) {
 		return "No quotes currently";
@@ -69,17 +45,19 @@ function formatQuotes(quotes, page) {
 	return str;
 }
 
+
 client.on("ready", () => {
 	console.log("Bot is online!");
 	console.log(`Logged in as ${client.user.tag}!`);
 
 	// TODO get this to work asynchronously
 	for (let g of client.guilds.cache) {
-		readSettings(g[0]);
+		settings.read(g[0]);
 	}
 });
+
 client.on("guildCreate", (guild) => {
-	readSettings(guild.id);
+	settings.read(guild.id);
 });
 client.on("guildDelete", (guild) => {
 	settings.delete(guild.id)
@@ -98,15 +76,15 @@ client.on("interactionCreate", async (interaction) => {
 			break;
 		case "schedule":
 			let schedule = interaction.options.getString("schedule");
-			getSettings(interaction.guildId).schedule = schedule;
-			saveSettings(interaction.guildId);
+			settings.get(interaction.guildId).schedule = schedule;
+			settings.save(interaction.guildId);
 
 			interaction.reply(`Beep boop (Changed schedule to \`${schedule}\`)`);
 			break;
 		case "channel":
 			let channel = interaction.options.getChannel("channel") == null ? interaction.channelId : interaction.options.getChannel("channel").id;
-			getSettings(interaction.guildId).channel = channel;
-			saveSettings(interaction.guildId);
+			settings.get(interaction.guildId).channel = channel;
+			settings.save(interaction.guildId);
 
 			interaction.reply(`Beep boop (Quotes will now be sent in <#${channel}>)`);
 			break;
@@ -120,17 +98,17 @@ client.on("interactionCreate", async (interaction) => {
 				break;
 			}
 
-			interaction.reply({ content: formatQuotes(getSettings(interaction.guildId).quotes, page), flags: Discord.MessageFlags.SuppressEmbeds });
+			interaction.reply({ content: formatQuotes(settings.get(interaction.guildId).quotes, page), flags: Discord.MessageFlags.SuppressEmbeds });
 			break;
 		case "add":
 			let quote = interaction.options.getString("quote");
-			getSettings(interaction.guildId).quotes.push(quote);
-			saveSettings(interaction.guildId);
+			settings.get(interaction.guildId).quotes.push(quote);
+			settings.save(interaction.guildId);
 
 			interaction.reply(`Beep boop (Added to list of quotes)`);
 			break;
 		case "remove":
-			if (getSettings(interaction.guildId).quotes.length === 0) {
+			if (settings.get(interaction.guildId).quotes.length === 0) {
 				interaction.reply(`Beep boop (No quotes to remove currently)`);
 				break;
 			}
@@ -141,22 +119,22 @@ client.on("interactionCreate", async (interaction) => {
 			} else if (index === -1) {
 				interaction.reply(`Beep boop (Indices start at 1)`);
 				break;
-			} else if (index > getSettings(interaction.guildId).quotes.length) {
+			} else if (index > settings.get(interaction.guildId).quotes.length) {
 				interaction.reply(`Beep boop (Please enter a number between 1 and ${index})`);
 				break;
 			} else {
-				let quote = getSettings(interaction.guildId).quotes.splice(index, 1);
-				saveSettings(interaction.guildId);
+				let quote = settings.get(interaction.guildId).quotes.splice(index, 1);
+				settings.save(interaction.guildId);
 
 				interaction.reply(`Beep boop (Removed quote ${index + 1}, _${quote[0]}_)`);
 			}
 			break;
 		case "quote":
-			if (getSettings(interaction.guildId).quotes.length < 1) {
+			if (settings.get(interaction.guildId).quotes.length < 1) {
 				interaction.reply(`Beep boop (Add a quote with \`/add\` first)`);
 				break;
 			}
-			interaction.reply(getSettings(interaction.guildId).quotes[Math.floor(Math.random() * getSettings(interaction.guildId).quotes.length)]);
+			interaction.reply(settings.get(interaction.guildId).quotes[Math.floor(Math.random() * settings.get(interaction.guildId).quotes.length)]);
 			break;
 	}
 });
